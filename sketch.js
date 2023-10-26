a21d119fddb09025685f7215d33a49fe8fa6d8c1
NyaNyong <sook9232@naver.com>
2023-10-27 03:29:55 +0900

    Force Accumulation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
-function setup() {
+class mover {
+  constructor() {
+    this.position = createVector(0,height/2);
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
+  }
+
+
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+    if (this.position.x > width) {
+      this.position.x = 0;
+    }
+  }
+  
+  display(c) {
+    fill(255,0,0);
+    circle(this.position.x,this.position.y,c);
+  }
+}
+let wind;
+
+function setup() { 
   createCanvas(400, 400);
+  M = new mover();
 }
 
 function draw() {
+  wind = createVector(0.05, 0);
   background(220);
+  M.update();
+  M.display(10);
+  if (mouseIsPressed === true) {
+    M.applyForce(wind);
+  }
 }
